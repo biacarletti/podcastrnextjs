f03fe1b075771d54a6d04ffd1e3f35facdb691b6
biacarletti <bia_carletti@hotmail.com>
2021-04-21 21:05:06 -0300

    terminando a aula 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-export default function Home() {
+//SPA disparar quando algo for modificado na aplicacao.
+//fetch ferramenta para fazer requisicao
+import { useEffect, useState } from "react"
+
+export default function Home(props) {
   return (
-   <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
